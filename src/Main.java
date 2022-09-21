019dbd9f6c7b2133b2bd854d7f7fb579ea608d20
Jaume Sánchez <jasato89@gmail.com>
2022-09-21 09:37:33 +0200

    Finished second exercise

--- src/Main.java
+++ src/Main.java
@@ -3,22 +3,46 @@ public class Main {
 
         int[] numbers = {1, 2, 5, 8, 3, 7, 9,-10, 23};
 
-        int result = differenceTwoSmallest(numbers);
+        int result = differenceSmallestAndLargest(numbers);
 
         System.out.println(result);
+
+        twoSmallest(numbers);
 
 
     }
 
-    private static int differenceTwoSmallest(int[] numbers) {
+    private static void twoSmallest(int[] numbers) {
 
         int smallest = Integer.MAX_VALUE;
-        int largest = Integer.MIN_VALUE;
+        int secondSmallest = Integer.MAX_VALUE;
+
+        if (numbers.length < 2) {
+            System.err.println("El array debe ser mayor que 1");
+        }
+
+        for (int number : numbers) {
+            if (number < smallest) {
+                secondSmallest = smallest;
+                smallest = number;
+            } else if(number < secondSmallest) {
+                secondSmallest = number;
+            }
+        }
+        System.out.println("El número más pequeño es " + smallest);
+        System.out.println("El segundo número más pequeño es " + secondSmallest);
+
+    }
+
+    private static int differenceSmallestAndLargest(int[] numbers) {
+
+        int smallestValue = Integer.MAX_VALUE;
+        int largestValue = Integer.MIN_VALUE;
 
         for(int i = 0; i < numbers.length; i++) {
-            if(numbers[i] < smallest) smallest = numbers[i];
-            if(numbers[i] > largest) largest = numbers[i];
+            if(numbers[i] < smallestValue) smallestValue = numbers[i];
+            if(numbers[i] > largestValue) largestValue = numbers[i];
         }
-        return largest - smallest;
+        return largestValue - smallestValue;
     }
 }
