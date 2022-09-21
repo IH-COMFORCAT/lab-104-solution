53867b66976fb73106f134e8f51aed645da3b570
Jaume Sánchez <jasato89@gmail.com>
2022-09-21 09:26:45 +0200

    Finished first exercise

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int[] numbers = {1, 2, 5, 8, 3, 7, 9,-10, 23};
+
+        int result = differenceTwoSmallest(numbers);
+
+        System.out.println(result);
+
+
+    }
+
+    private static int differenceTwoSmallest(int[] numbers) {
+
+        int smallest = Integer.MAX_VALUE;
+        int largest = Integer.MIN_VALUE;
+
+        for(int i = 0; i < numbers.length; i++) {
+            if(numbers[i] < smallest) smallest = numbers[i];
+            if(numbers[i] > largest) largest = numbers[i];
+        }
+        return largest - smallest;
     }
 }
